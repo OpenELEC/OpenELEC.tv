f67a4c964e5ccb6cc3d6ebfb8c8595ede73585c1
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-01-31 23:11:26 +0100

    Add quicknes core (segfaulting)

--- packages/RPi-Lakka/quicknes/package.mk
+++ packages/RPi-Lakka/quicknes/package.mk
@@ -18,19 +18,28 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="RPi-Lakka"
-PKG_VERSION=""
+PKG_NAME="quicknes"
+PKG_VERSION="2d7790e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/Niouby/OpenELEC.tv"
-PKG_URL=""
-PKG_DEPENDS="RetroArch pocketsnes-libretro genesis-plus-gx nxengine fceu-next gambatte stella imame4all vbam-libretro mednafen-gba vba-next meteor nestopia quicknes retroarch-joypad-autoconfig"
-PKG_BUILD_DEPENDS=""
+PKG_SITE="https://github.com/libretro/QuickNES_Core"
+PKG_URL="http://localhost/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
-PKG_SECTION="RPi-Lakka"
-PKG_SHORTDESC="RPi-Lakka metapackage"
-PKG_LONGDESC=""
+PKG_SECTION="RetroArch"
+PKG_SHORTDESC="The QuickNES core library, originally by Shay Green, heavily modified"
+PKG_LONGDESC="The QuickNES core library, originally by Shay Green, heavily modified"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+make_target() {
+  make -C libretro
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp libretro/quicknes_libretro.so $INSTALL/usr/lib/libretro/libretro-quicknes.so
+}
