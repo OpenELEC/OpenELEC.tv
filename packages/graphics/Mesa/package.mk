86a51680b6644ecb099a03c6855da978e91b6b4d
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-05-15 16:26:10 +0200

    Remove useless nvidia stuff

--- packages/graphics/Mesa/package.mk
+++ packages/graphics/Mesa/package.mk
@@ -107,11 +107,11 @@ pre_configure_target() {
   strip_gold
 }
 
-post_makeinstall_target() {
-  # rename and relink for cooperate with nvidia drivers
-    rm -rf $INSTALL/usr/lib/libGL.so
-    rm -rf $INSTALL/usr/lib/libGL.so.1
-    ln -sf libGL.so.1 $INSTALL/usr/lib/libGL.so
-    ln -sf /var/lib/libGL.so $INSTALL/usr/lib/libGL.so.1
-    mv $INSTALL/usr/lib/libGL.so.1.2.0 $INSTALL/usr/lib/libGL_mesa.so.1
-}
+#post_makeinstall_target() {
+#  # rename and relink for cooperate with nvidia drivers
+#    rm -rf $INSTALL/usr/lib/libGL.so
+#    rm -rf $INSTALL/usr/lib/libGL.so.1
+#    ln -sf libGL.so.1 $INSTALL/usr/lib/libGL.so
+#    ln -sf /var/lib/libGL.so $INSTALL/usr/lib/libGL.so.1
+#    mv $INSTALL/usr/lib/libGL.so.1.2.0 $INSTALL/usr/lib/libGL_mesa.so.1
+#}
