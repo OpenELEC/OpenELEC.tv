26db2d020be49cca5d63ecb6bb81b97e90291151
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-06-04 11:59:33 +0200

    Add support for Neo Geo Pocket

--- packages/lakka/mednafen-ngp/package.mk
+++ packages/lakka/mednafen-ngp/package.mk
@@ -18,24 +18,28 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="Generic"
-PKG_VERSION=""
+PKG_NAME="mednafen-ngp"
+PKG_VERSION="0e81bd9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/Niouby/OpenELEC.tv"
-PKG_URL=""
-PKG_DEPENDS_TARGET="Mesa RetroArch mednafen-ngp mednafen-pcfx mame mednafen-psx mupen64plus vecx snes9x-next dinothawr prboom mednafen-pce fba handy genesis-plus-gx nxengine fceu-next gambatte stella vbam libretro-ffmpeg retroarch-joypad-autoconfig core-info"
-PKG_BUILD_DEPENDS=""
+PKG_SITE="https://github.com/libretro/mednafen-libretro"
+PKG_URL="$LAKKA_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
-PKG_SECTION="virtual"
-PKG_SHORTDESC="Lakka metapackage for Generic"
-PKG_LONGDESC=""
+PKG_SECTION="RetroArch"
+PKG_SHORTDESC="libretro implementation of Mednafen Neo Geo Pocket."
+PKG_LONGDESC="libretro implementation of Mednafen Neo Geo Pocket."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-if [ "$SAMBA_SUPPORT" = yes ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET samba"
-fi
+make_target() {
+  make core=ngp
+}
 
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp mednafen_ngp_libretro.so $INSTALL/usr/lib/libretro/
+}
