5073861fb06762f6bc68c04a729b60e22311ae36
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-06-11 21:33:27 +0200

    Add Desmume core

--- packages/lakka/desmume/package.mk
+++ packages/lakka/desmume/package.mk
@@ -18,24 +18,28 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="Generic"
-PKG_VERSION=""
+PKG_NAME="desmume"
+PKG_VERSION="1dd58e4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/Niouby/OpenELEC.tv"
-PKG_URL=""
-PKG_DEPENDS_TARGET="Mesa RetroArch desmume mednafen-ngp mednafen-pcfx mame mednafen-psx mupen64plus vecx snes9x-next dinothawr prboom mednafen-pce fba handy genesis-plus-gx nxengine fceu-next gambatte stella vbam libretro-ffmpeg retroarch-joypad-autoconfig core-info"
-PKG_BUILD_DEPENDS=""
+PKG_SITE="https://github.com/libretro/desmume-libretro"
+PKG_URL="$LAKKA_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
-PKG_SECTION="virtual"
-PKG_SHORTDESC="Lakka metapackage for Generic"
-PKG_LONGDESC=""
+PKG_SECTION="RetroArch"
+PKG_SHORTDESC="libretro wrapper for desmume NDS emulator."
+PKG_LONGDESC="libretro wrapper for desmume NDS emulator."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-if [ "$SAMBA_SUPPORT" = yes ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET samba"
-fi
+make_target() {
+  make -f Makefile.libretro
+}
 
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp desmume_libretro.so $INSTALL/usr/lib/libretro/
+}
