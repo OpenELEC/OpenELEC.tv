292bfe23cc0d6dea33a1b315b738ee1ce1c60bca
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-06-17 18:34:02 +0200

    Update RetroArch and core-info

--- packages/lakka/RetroArch/package.mk
+++ packages/lakka/RetroArch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="RetroArch"
-PKG_VERSION="c5c6223"
+PKG_VERSION="270bb93"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
--- packages/lakka/core-info/package.mk
+++ packages/lakka/core-info/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="8d2c1cf"
+PKG_VERSION="929c315"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
