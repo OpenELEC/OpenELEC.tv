7e63e9aa20642e383d8fd2253336953f4b48ac50
Alex Deryskyba <alex@codesnake.com>
2014-07-09 09:52:13 +0300

    Update to GCC-4.7.4

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="gcc"
-PKG_VERSION="4.7.3"
+PKG_VERSION="4.7.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
