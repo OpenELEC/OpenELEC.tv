2f16c84ca897711c3cd6de8c0021956659618d12
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-07-20 16:55:55 +0200

    Update mupen64plus

--- packages/lakka/mupen64plus/package.mk
+++ packages/lakka/mupen64plus/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mupen64plus"
-PKG_VERSION="b28bb4e"
+PKG_VERSION="bf2aede"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
