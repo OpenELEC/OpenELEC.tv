cd28303960ed8a11801ff334efa1efa86054a2ee
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-07-23 00:37:42 +0200

    Update core-info

--- packages/lakka/core-info/package.mk
+++ packages/lakka/core-info/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="929c315"
+PKG_VERSION="f9adfe9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
