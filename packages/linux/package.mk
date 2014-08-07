6f42387c8b6f1928d0f86eb521612c0406ad819e
Stephan Raue <stephan@openelec.tv>
2014-08-07 16:31:26 +0200

    linux: remove kernel 3.15 support
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -17,16 +17,8 @@
 ################################################################################
 
 PKG_NAME="linux"
-case "$LINUX" in
-  3.15)
-    PKG_VERSION="3.15.8"
-    PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
-    ;;
-  *)
-    PKG_VERSION="3.16"
-    PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
-    ;;
-esac
+PKG_VERSION="3.16"
+PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
