09ab382736724b68afa0feb67d93456bc32f2297
Stephan Raue <stephan@openelec.tv>
2014-08-18 01:31:15 +0200

    xbmc: update to xbmc-13-0f3db05
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc"
-PKG_VERSION="13-f2ddaae"
+PKG_VERSION="13-0f3db05"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
