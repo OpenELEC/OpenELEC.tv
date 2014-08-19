e21872c8cd24b36d5fafba1834c6dedea436f790
Floréal Toumikian <floreal@april.org>
2014-08-19 09:10:30 +0200

    Updated ppsspp

--- packages/lakka/ppsspp/package.mk
+++ packages/lakka/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="baebf5e"
+PKG_VERSION="33cf19d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
