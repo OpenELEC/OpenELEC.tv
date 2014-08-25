5a422bfaadee60532e10b68f563fcb8bcb6de596
Floréal Toumikian <floreal@nimukaito.net>
2014-08-25 21:20:37 +0200

    Updated: ppsspp

--- packages/lakka/ppsspp/package.mk
+++ packages/lakka/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="63fa4fc"
+PKG_VERSION="62e5df3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
