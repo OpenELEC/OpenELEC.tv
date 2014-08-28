2bfcff9ffb6f42979ffa97acb58135ae38eb3308
Floréal Toumikian <floreal@nimukaito.net>
2014-08-28 21:44:51 +0200

    Fix: Restored former version of libretro-ffmpeg, updated core-info

--- packages/lakka/core-info/package.mk
+++ packages/lakka/core-info/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="024bc26"
+PKG_VERSION="4ea6a59"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
