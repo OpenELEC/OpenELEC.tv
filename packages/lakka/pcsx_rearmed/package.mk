1884a419cd9b575bf40592a7f2199807679bb42a
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-10-04 16:06:22 +0200

    Update pcsx_rearmed

--- packages/lakka/pcsx_rearmed/package.mk
+++ packages/lakka/pcsx_rearmed/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="1929a79"
+PKG_VERSION="eae1ae0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
