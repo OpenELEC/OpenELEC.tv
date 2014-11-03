3786f089be92a7adac10e3452c83966fcb0eefdd
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-11-04 00:43:44 +0100

    Update RetroArch

--- packages/lakka/RetroArch/package.mk
+++ packages/lakka/RetroArch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="RetroArch"
-PKG_VERSION="018a45e"
+PKG_VERSION="4cd4ce9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
