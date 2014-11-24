62e0c0201929002ae6cd8da59b98253a55a588c8
Alex Deryskyba <alex@codesnake.com>
2014-11-24 04:36:17 +0200

    [aml] Update to Linux kernel 3.10.40

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="linux"
 case "$LINUX" in
   amlogic)
-    LINUX_VERSION="3.10.35"
+    LINUX_VERSION="3.10.40"
     PKG_VERSION="amlogic-$LINUX_VERSION"
     PKG_URL="https://github.com/codesnake/linux-amlogic/releases/download/$LINUX_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
