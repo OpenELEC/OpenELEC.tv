e7364c3a55bf9eaf0c7b951eff58d4152aad9000
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-01-12 17:49:44 +0100

    Update ppsspp

--- packages/lakka/ppsspp/package.mk
+++ packages/lakka/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="cd01945"
+PKG_VERSION="5ccd9cc"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
