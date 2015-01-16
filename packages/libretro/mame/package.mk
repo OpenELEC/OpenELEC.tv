690bef0d8bce15b3d1fc604d130f9a928c757a71
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-01-16 15:42:30 +0100

    Update MAME

--- packages/libretro/mame/package.mk
+++ packages/libretro/mame/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mame"
-PKG_VERSION="4ad8667"
+PKG_VERSION="db97c05"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -36,10 +36,11 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_target() {
+
   sed -i -e "s/\tCC_AS = gcc/\tCC_AS = \$\(CC\)/" Makefile.libretro
   sed -i -e "s/\tCC = g++//" Makefile.libretro
   sed -i -e "s/\tNATIVECC = g++/\tNATIVECC = \$\(CXX\)/" Makefile.libretro
-  #sed -i -e "s/\tAR = @ar//" Makefile.libretro
+  sed -i -e "s/\tAR = @ar//" Makefile.libretro
   sed -i -e "s/\tLD = g++//" Makefile.libretro
   sed -i -e "s/\tNATIVELD = g++/\tNATIVELD = \$\(CXX\)/" Makefile.libretro
   #sed -i -e "s/-Wl,--version-script=src\/osd\/retro\/link.T//" Makefile.libretro
