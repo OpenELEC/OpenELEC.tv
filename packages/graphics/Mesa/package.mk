7e0714feb743763c9c422df6105c07d92e08496b
Stephan Raue <stephan@openelec.tv>
2015-01-24 17:08:31 +0100

    Mesa: update to Mesa-10.3.7
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/Mesa/package.mk
+++ packages/graphics/Mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="Mesa"
-PKG_VERSION="10.3.6"
+PKG_VERSION="10.3.7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
