201232123a3f62799266d62187221b8749ac3e93
Wesley Ellis <wesley.ellis@webanywhere.co.uk>
2015-03-13 11:19:07 +0000

    kodi: update to kodi-14-e7ba06f

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="14-085163e"
+PKG_VERSION="14-e7ba06f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
