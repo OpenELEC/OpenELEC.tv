caeae1a152ffd23964f6da91b3ffbb99ce640bef
Lukas Rusak <lorusak@gmail.com>
2015-03-25 13:14:27 -0700

    libva: allow building with DISPLAYSERVER=wayland

--- packages/multimedia/libva/package.mk
+++ packages/multimedia/libva/package.mk
@@ -32,13 +32,18 @@ PKG_LONGDESC="The main motivation for VAAPI (Video Acceleration API) is to enabl
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
+if [ "$DISPLAYSERVER" = "x11" ]; then
+  PKG_DEPENDS_TARGET="toolchain libX11 libXext libXfixes libdrm mesa glu"
+  DISPLAYSERVER_LIBVA="--enable-x11 --enable-glx --disable-egl --disable-wayland"
+elif [ "$DISPLAYSERVER" = "wayland" ]; then
+  DISPLAYSERVER_LIBVA="--disable-x11 --disable-glx --enable-egl --enable-wayland"
+  PKG_DEPENDS_TARGET="toolchain libdrm mesa wayland"
+fi
+
 PKG_CONFIGURE_OPTS_TARGET="--disable-silent-rules \
                            --disable-docs \
                            --enable-drm \
-                           --enable-x11 \
-                           --enable-glx \
-                           --disable-egl \
-                           --disable-wayland \
+                           $DISPLAYSERVER_LIBVA \
                            --disable-dummy-driver \
                            --with-drivers-path=/usr/lib/va"
 
