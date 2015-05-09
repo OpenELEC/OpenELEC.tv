a5310d6344c6cbe0b708646206c25e8fd7ea9d61
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-05-09 05:54:38 +0200

    Update joypad configs

--- packages/libretro/retroarch-joypad-autoconfig/package.mk
+++ packages/libretro/retroarch-joypad-autoconfig/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="8f7dffb"
+PKG_VERSION="30ff89c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
