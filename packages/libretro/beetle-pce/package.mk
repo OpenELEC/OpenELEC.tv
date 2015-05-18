d55ab5564de84260800a74981f4145d52d3202aa
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-05-18 11:35:18 +0200

    Upgrade beetle-pce

--- packages/libretro/beetle-pce/package.mk
+++ packages/libretro/beetle-pce/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-pce"
-PKG_VERSION="4452587"
+PKG_VERSION="3567dee"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
