624d5974282377371b3ebf5829880b480717289c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-05-26 00:20:49 +0200

    Update nestopia

--- packages/libretro/nestopia/package.mk
+++ packages/libretro/nestopia/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="nestopia"
-PKG_VERSION="2c586b5"
+PKG_VERSION="8836c53"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
