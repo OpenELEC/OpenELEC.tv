43f71a841c9ccb673257415be5bbe718ed395a77
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-05-29 16:32:52 +0200

    Add KADE joypad config

--- packages/libretro/retroarch-joypad-autoconfig/package.mk
+++ packages/libretro/retroarch-joypad-autoconfig/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="01cb9d9"
+PKG_VERSION="10d8dcf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
