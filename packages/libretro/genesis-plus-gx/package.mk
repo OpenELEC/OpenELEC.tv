eac1dc4f6e5611c324a639fc92e4999eb31b3ab7
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-06-02 16:40:17 +0200

    Update Genesis-plus-gx

--- packages/libretro/genesis-plus-gx/package.mk
+++ packages/libretro/genesis-plus-gx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx"
-PKG_VERSION="c0015e2"
+PKG_VERSION="d291c54"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
