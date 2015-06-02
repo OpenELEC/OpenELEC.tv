bde9debce78efffa4863bbe63e8aece4ea9fe103
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-06-02 16:40:32 +0200

    Update FBA

--- packages/libretro/fba/package.mk
+++ packages/libretro/fba/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fba"
-PKG_VERSION="67c2eb7"
+PKG_VERSION="cfa628c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
