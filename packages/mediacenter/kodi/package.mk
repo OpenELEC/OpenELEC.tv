f459c6a7ed073a20e9686fe60d200e034fc51b74
alex <surkov.alex@gmail.com>
2015-06-20 00:09:05 +0300

    [aml] update kodi to 15.0-rc1-f806955

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="15.0-beta2-688813a"
+PKG_VERSION="15.0-rc1-f806955"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
