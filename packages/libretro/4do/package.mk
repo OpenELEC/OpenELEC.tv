58d6a6561d7dbac1df82389e21119f5d9193540f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-27 01:17:45 +0200

    Update 4do

--- packages/libretro/4do/package.mk
+++ packages/libretro/4do/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="4do"
-PKG_VERSION="46cc2a9"
+PKG_VERSION="fa1a1e9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL with additional notes"
