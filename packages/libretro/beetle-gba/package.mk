a0f898a334ec7a277478dc413b4265b306f44896
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-27 01:21:49 +0200

    Update beetle-gba

--- packages/libretro/beetle-gba/package.mk
+++ packages/libretro/beetle-gba/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-gba"
-PKG_VERSION="bc0b54a"
+PKG_VERSION="2dcde8b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
