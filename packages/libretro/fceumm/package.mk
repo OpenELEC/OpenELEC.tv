4eec7c2727e961d3d8e6d4bffcd5a2cecc39b10a
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-27 13:18:06 +0200

    Update fceumm

--- packages/libretro/fceumm/package.mk
+++ packages/libretro/fceumm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fceumm"
-PKG_VERSION="46ac3a8"
+PKG_VERSION="e3794ef"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
