a070dc2a276c81178e1a4673015fe257f3d17894
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-27 13:18:35 +0200

    Update fuse

--- packages/libretro/fuse-libretro/package.mk
+++ packages/libretro/fuse-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fuse-libretro"
-PKG_VERSION="0984e78"
+PKG_VERSION="98a590f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
