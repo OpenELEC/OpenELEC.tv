a8c89c2f6ca4ce95c3c69c0aacb8e2c7799f9b1c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-27 21:53:18 +0200

    Update nxengine

--- packages/libretro/nxengine/package.mk
+++ packages/libretro/nxengine/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="nxengine"
-PKG_VERSION="f631338"
+PKG_VERSION="381af8c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
