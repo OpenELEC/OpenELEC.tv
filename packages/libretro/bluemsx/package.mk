1ef12bc13d3e234d9995248d04e8eb5d3928094c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-27 02:03:38 +0200

    Update bluemsx

--- packages/libretro/bluemsx/package.mk
+++ packages/libretro/bluemsx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="bluemsx"
-PKG_VERSION="c8c9ce8"
+PKG_VERSION="bf43517"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
