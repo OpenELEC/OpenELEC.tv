785d51df13915fca9e8b9b7314a0b9e262c1dff0
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-28 01:41:08 +0200

    Update stella

--- packages/libretro/stella/package.mk
+++ packages/libretro/stella/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="stella"
-PKG_VERSION="c7ee7ca"
+PKG_VERSION="762f6a8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
