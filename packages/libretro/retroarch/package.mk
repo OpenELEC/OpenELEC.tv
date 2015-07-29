2f63d74b1f82e4e8832af4d2972f74cf17f94e26
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-07-30 00:33:04 +0200

    Update RetroArch, fixes udev autoconfig saving

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="1bd450f"
+PKG_VERSION="77245e9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
