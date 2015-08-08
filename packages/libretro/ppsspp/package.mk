548df5c71c1c1a54a6d782a04dfbb2b3c1d88e80
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-08-08 21:54:48 +0200

    Update ppsspp

--- packages/libretro/ppsspp/package.mk
+++ packages/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="6a766cf"
+PKG_VERSION="79f587e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
