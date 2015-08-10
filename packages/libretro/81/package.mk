e0d427a03bbd72880169918c92d826f56004d450
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-08-10 15:17:19 +0200

    Package 81 and enable it in the generic build

--- packages/libretro/81/package.mk
+++ packages/libretro/81/package.mk
@@ -18,19 +18,27 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="Generic"
-PKG_VERSION=""
+PKG_NAME="81"
+PKG_VERSION="cf44eca"
 PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/lakkatv/Lakka"
-PKG_URL=""
-PKG_DEPENDS_TARGET="retroarch 81 mame yabause hatari dosbox mame2003 gw-libretro fuse-libretro scummvm bluemsx beetle-sgx lutro 4do prosystem bsnes-mercury tyrquake 2048 virtualjaguar ppsspp beetle-vb beetle-wswan desmume beetle-ngp beetle-pcfx beetle-psx mupen64plus vecx snes9x-next dinothawr prboom beetle-pce fba handy genesis-plus-gx nxengine nestopia gambatte stella vbam libretro-ffmpeg"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/81-libretro"
+PKG_URL="$LAKKA_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
-PKG_SECTION="virtual"
-PKG_SHORTDESC="Lakka metapackage for Generic"
-PKG_LONGDESC=""
+PKG_SECTION="libretro"
+PKG_SHORTDESC="A port of the EightyOne ZX81 Emulator to libretro"
+PKG_LONGDESC="A port of the EightyOne ZX81 Emulator to libretro"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+make_target() {
+  make -f Makefile.libretro
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp 81_libretro.so $INSTALL/usr/lib/libretro/
+}
