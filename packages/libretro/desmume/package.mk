7d750a7d411c18540c0e150b4cf1636c5e19e5af
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-08-11 22:38:43 +0200

    Update desmume

--- packages/libretro/desmume/package.mk
+++ packages/libretro/desmume/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="desmume"
-PKG_VERSION="ed0e1b8"
+PKG_VERSION="30aea8b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
