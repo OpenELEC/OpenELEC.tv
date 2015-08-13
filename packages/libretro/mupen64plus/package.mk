e1fb88d7ffdd22551793eb65f0901473daa9fe17
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-08-13 13:27:25 +0200

    Update mupen64plus

--- packages/libretro/mupen64plus/package.mk
+++ packages/libretro/mupen64plus/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mupen64plus"
-PKG_VERSION="32b9ad5"
+PKG_VERSION="75e01f1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
