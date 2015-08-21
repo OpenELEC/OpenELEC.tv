9ee7608972cf264ab8bc5440d1b8e5d3212f5baa
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-08-21 23:13:50 +0200

    snes9x-next fix

--- packages/libretro/snes9x-next/package.mk
+++ packages/libretro/snes9x-next/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x-next"
-PKG_VERSION="76aca1f"
+PKG_VERSION="0e7fa64"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
