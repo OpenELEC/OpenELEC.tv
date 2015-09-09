84178c4800b52e2f30852860963aeb8582267bab
islipfd19 <islipfd19@gmail.com>
2015-09-09 13:05:58 -0400

    Update package.mk

--- packages/textproc/jsoncpp/package.mk
+++ packages/textproc/jsoncpp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="jsoncpp"
-PKG_VERSION="0.6.5"
+PKG_VERSION="1.6.5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
