567bc8f80b7575aa40cc38bf066c3b54b00b1655
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-09-23 20:05:43 +0200

    Add the script used for nightly builds

---
+++ nightlies.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+rm -rf target/
+
+DISTRO=Lakka PROJECT=RPi ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=RPi ARCH=arm make noobs -j9
+DISTRO=Lakka PROJECT=RPi2 ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=RPi2 ARCH=arm make noobs -j9
+DISTRO=Lakka PROJECT=imx6 ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=Generic ARCH=x86_64 make image -j9
+DISTRO=Lakka PROJECT=Generic ARCH=i386 make image -j9
+DISTRO=Lakka PROJECT=a20 ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=a10 ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=Bananapi ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=OdroidC1 ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=OdroidXU3 ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=WeTek_Play ARCH=arm make image -j9
+DISTRO=Lakka PROJECT=WandBoard ARCH=arm make image -j9
+
+for f in target/*; do
+  md5sum $f > $f.md5
+  sha256sum $f > $f.sha256
+done
+
+for f in target/*; do
+  dir=`echo $f | sed -e 's/target\/Lakka-\(.*\)-devel-\(.*\)/\1/'`
+  mkdir -p target/$dir
+  mv $f target/$dir/
+done
+
+#scp -r target/* lakka@sources.lakka.tv:sources/nightly/
