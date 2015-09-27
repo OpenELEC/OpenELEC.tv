a11c890be68a58a8dde672722eae64c23e1f4ea8
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-09-28 01:06:53 +0200

    Update RetroArch, add psx and psp scanning

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="19fb524"
+PKG_VERSION="6d97163"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
