df086933519f350deb101f0d34ee6b2a41a39505
fritsch <peter.fruehberger@gmail.com>
2015-10-01 07:56:13 +0200

    Intel: Passthrough Mode - little workaround

--- projects/Generic/filesystem/usr/bin/intel-fullrange.sh
+++ projects/Generic/filesystem/usr/bin/intel-fullrange.sh
@@ -4,6 +4,8 @@ FB_TYPE="$(grep '^0 ' /proc/fb | sed 's/[^[:space:]] //')"
 if [ "$FB_TYPE" == "inteldrmfb" ]; then
   OUTPUT=`/usr/bin/xrandr -display :0 -q | sed '/ connected/!d;s/ .*//;q'`
   for out in $OUTPUT ; do
+    # Hack - something is not yet fully right
+    /usr/bin/xrandr -display :0 --output $out --set "Broadcast RGB" "Full"
     /usr/bin/xrandr -display :0 --output $out --set "Broadcast RGB" "Video 16:235 pass-through"
   done
 fi
