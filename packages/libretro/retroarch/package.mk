2e473568adaef14c6c3e44bfdd6bafe77719d786
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-10-19 07:08:26 +0200

    Update retroarch

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="6d97163"
+PKG_VERSION="69b9572"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
