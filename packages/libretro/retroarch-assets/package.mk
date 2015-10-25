9aeb50e74068b66647fdefcd954181538433b083
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-10-25 12:47:48 +0100

    Update RetroArch, add a new tab for easy scanning

--- packages/libretro/retroarch-assets/package.mk
+++ packages/libretro/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="3bc59e3"
+PKG_VERSION="ec3e154"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="c0878c7"
+PKG_VERSION="640de49"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
