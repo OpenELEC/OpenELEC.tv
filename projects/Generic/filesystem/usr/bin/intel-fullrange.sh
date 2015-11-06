ad832f62d635d6c4561e143925150bf8a7982dfc
fritsch <Peter.Fruehberger@gmail.com>
2015-11-06 08:06:49 +0100

    ColorRangeHack: Allow off and on by adding /storage/.config/forcedisplay

--- projects/Generic/filesystem/usr/bin/intel-fullrange.sh
+++ projects/Generic/filesystem/usr/bin/intel-fullrange.sh
@@ -7,5 +7,10 @@ if [ "$FB_TYPE" == "inteldrmfb" ]; then
     # Hack - something is not yet fully right
     /usr/bin/xrandr -display :0 --output $out --set "Broadcast RGB" "Full"
     /usr/bin/xrandr -display :0 --output $out --set "Broadcast RGB" "Video 16:235 pass-through"
+    # Seems there is a little race somewhere on some outputs
+    # Turn the display shortly off and on again
+    if [ -e "/storage/.config/forcedisplay" ]; then
+      /usr/bin/xrandr -display :0 --output $out --off ; /usr/bin/xrandr -display :0 --output $out --auto
+    fi
   done
 fi
