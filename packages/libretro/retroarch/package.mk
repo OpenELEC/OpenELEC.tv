2da5752ed01f9bc72f52a1409e154f1cd8c8d131
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-11-24 21:25:51 +0100

    Upgrade RetroArch, new data runloop

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="62a556e"
+PKG_VERSION="1186042"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
