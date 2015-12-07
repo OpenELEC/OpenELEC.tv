aa8871ae3607bb971c7b333571c9845f4e3ea2ec
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-12-07 05:09:43 +0100

    Update assets

--- packages/libretro/retroarch-assets/package.mk
+++ packages/libretro/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="d77e609"
+PKG_VERSION="ebc3789"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
