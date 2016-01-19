45c434b0649a4d1360e6108f5fbc0daaf4603a53
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-01-19 17:27:36 +0100

    Update gpsp

--- packages/libretro/gpsp/package.mk
+++ packages/libretro/gpsp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="gpsp"
-PKG_VERSION="4036107"
+PKG_VERSION="4b00c74"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
