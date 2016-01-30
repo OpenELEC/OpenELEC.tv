437ec7a5f324fd79bee05dd51d06bf99f2f28580
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-01-30 17:05:00 +0100

    Update virtualjaguar

--- packages/libretro/virtualjaguar/package.mk
+++ packages/libretro/virtualjaguar/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="virtualjaguar"
-PKG_VERSION="499eca6"
+PKG_VERSION="424ce6e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
