81d946cf47bed47afdbcd7e3d29fee067bf758f6
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-02-15 02:20:51 +0100

    Update beetle-psx, bugfixes

--- packages/libretro/beetle-psx/package.mk
+++ packages/libretro/beetle-psx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-psx"
-PKG_VERSION="b8d5a6f"
+PKG_VERSION="3e6eead"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
