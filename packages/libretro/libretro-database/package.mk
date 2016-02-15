f76ec43791ea333dcdc47606fd61ef8e65b25533
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-02-15 02:37:20 +0100

    Update libretro-database, start supporting tgdb metadata

--- packages/libretro/libretro-database/package.mk
+++ packages/libretro/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="59eb194"
+PKG_VERSION="e3a7445"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
