5da3af520a88b48b0a2a8d45699897e05cde8d06
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-02-21 18:48:29 +0100

    Update beetle-psx, fix deinterlacing on extended resolutions

--- packages/libretro/beetle-psx/package.mk
+++ packages/libretro/beetle-psx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-psx"
-PKG_VERSION="3e6eead"
+PKG_VERSION="d9dd6c2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
