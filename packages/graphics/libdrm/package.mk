f4a1701b749383638c40be6980d314468e612dca
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-03-02 10:51:15 +0100

    Update libdrm

--- packages/graphics/libdrm/package.mk
+++ packages/graphics/libdrm/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libdrm"
-PKG_VERSION="2.4.65"
+PKG_VERSION="2.4.67"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
