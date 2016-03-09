8bb43d522c750b61e9c65a3b0afd3a05a5cb67ec
Lukas Rusak <lorusak@gmail.com>
2016-03-09 21:29:06 +0100

    kodi: use max kellerman fork

--- packages/mediacenter/kodi-theme-Estuary/package.mk
+++ packages/mediacenter/kodi-theme-Estuary/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Estuary"
-PKG_VERSION="17.0-alpha1-c6b201a"
+PKG_VERSION="17.0-alpha1-fd25bd4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
