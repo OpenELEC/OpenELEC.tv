63b76c50cd9fcbd3ea688f130ff3257f9883977f
Stephan Raue <stephan@openelec.tv>
2016-03-15 13:11:27 +0100

    pvr.mediaportal.tvserver: use git checkout, update to pvr.mediaportal.tvserver-180af88
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/pvr.mediaportal.tvserver/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.mediaportal.tvserver/package.mk
@@ -17,12 +17,13 @@
 ################################################################################
 
 PKG_NAME="pvr.mediaportal.tvserver"
-PKG_VERSION="9dd54a6"
+PKG_VERSION="180af88"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_GIT_URL="https://github.com/kodi-pvr/pvr.mediaportal.tvserver"
+PKG_GIT_BRANCH="master"
 PKG_DEPENDS_TARGET="toolchain kodi-platform"
 PKG_PRIORITY="optional"
 PKG_SECTION=""
