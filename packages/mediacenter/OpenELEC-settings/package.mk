14042c999c9c29ee9ec8727b5b924288692db8be
Stephan Raue <stephan@openelec.tv>
2016-03-15 15:56:36 +0100

    OpenELEC-settings: use git checkout, update to OpenELEC-settings-91548b5
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/OpenELEC-settings/package.mk
+++ packages/mediacenter/OpenELEC-settings/package.mk
@@ -17,12 +17,13 @@
 ################################################################################
 
 PKG_NAME="OpenELEC-settings"
-PKG_VERSION="0.6.12"
+PKG_VERSION="91548b5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
 PKG_SITE="http://www.openelec.tv"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_GIT_URL="https://github.com/OpenELEC/service.openelec.settings.git"
+PKG_GIT_BRANCH="master"
 PKG_DEPENDS_TARGET="toolchain Python connman pygobject dbus-python"
 PKG_PRIORITY="optional"
 PKG_SECTION=""
