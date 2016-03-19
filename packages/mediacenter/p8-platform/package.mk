a6ddcba8a3367f75b59c13dd61cc7f55bb4d9d69
Stephan Raue <stephan@openelec.tv>
2016-03-19 11:54:02 +0100

    platform: rename package to 'p8-platform'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/p8-platform/package.mk
+++ packages/mediacenter/p8-platform/package.mk
@@ -16,8 +16,8 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="platform"
-PKG_VERSION="081032f"
+PKG_NAME="p8-platform"
+PKG_VERSION="38343e0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -27,8 +27,8 @@ PKG_GIT_BRANCH="master"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
-PKG_SHORTDESC="platform:"
-PKG_LONGDESC="platform:"
+PKG_SHORTDESC="Platform support library used by libCEC and binary add-ons for Kodi"
+PKG_LONGDESC="Platform support library used by libCEC and binary add-ons for Kodi"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
