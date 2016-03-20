f84557f1fbdd3e6c064c967e9c8dc16932fd728a
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-03-20 09:47:25 +0100

    Update wetekdvb

--- packages/linux-drivers/wetekdvb/package.mk
+++ packages/linux-drivers/wetekdvb/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wetekdvb"
-PKG_VERSION="20151007"
+PKG_VERSION="20160307"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
