97624dc82b3be3b7523231518e0295adec3f5ae6
Stephan Raue <stephan@openelec.tv>
2016-03-22 19:37:09 +0100

    audiodecoder.wsr: update to audiodecoder.wsr-907d964, fix PKG_GIT_URL
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/audiodecoder.wsr/package.mk
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.wsr/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="audiodecoder.wsr"
-PKG_VERSION="f5aff29"
+PKG_VERSION="907d964"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/notspiff/audiodecoder.wsr"
-PKG_GIT_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_GIT_URL="https://github.com/notspiff/audiodecoder.wsr"
 PKG_GIT_BRANCH="master"
 PKG_DEPENDS_TARGET="toolchain kodi-platform"
 PKG_PRIORITY="optional"
