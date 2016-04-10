29a826b7f2552f030c4bdd37dca2b1109a0189a3
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-04-10 10:01:34 +0200

    Update RetroArch. Adds support for the RetroActive XMB theme. Improve the look of XMB drop shadows.

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="7086277"
+PKG_VERSION="d818578"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
