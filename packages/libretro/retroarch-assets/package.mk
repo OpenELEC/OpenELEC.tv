597947c0b4d3df52783ea21c637756f9d9308c74
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-04-10 10:00:05 +0200

    Add the RetroActive XMB theme

--- packages/libretro/retroarch-assets/package.mk
+++ packages/libretro/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="47f7d33"
+PKG_VERSION="8b44bc1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
