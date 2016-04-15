c54fc5ca0dfa444abdbdde5f12e13054e279dd57
Stephan Raue <stephan@openelec.tv>
2016-04-15 09:55:36 +0200

    linux: update to linux-4.5.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -41,7 +41,7 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan"
     ;;
   *)
-    PKG_VERSION="4.5"
+    PKG_VERSION="4.5.1"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
 esac
