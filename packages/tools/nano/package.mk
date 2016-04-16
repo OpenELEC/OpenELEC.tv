f08a9c6c1e4e30d58a69c602a9bed2aac919407f
Stephan Raue <stephan@openelec.tv>
2016-04-16 18:22:09 +0200

    nano: enable utf8 support, cleanup
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/tools/nano/package.mk
+++ packages/tools/nano/package.mk
@@ -32,14 +32,7 @@ PKG_LONGDESC="GNU nano (Nano's ANOther editor, or Not ANOther editor) is an enha
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-utf8 \
-                           --disable-nls"
-
-pre_configure_target() {
-#  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/ncurses"
-  export LDFLAGS=`echo $LDFLAGS | sed -e "s|-Wl,--as-needed||"`
-  export LIBS="$LIBS -lz"
-}
+PKG_CONFIGURE_OPTS_TARGET="--enable-utf8 --disable-nls --disable-libmagic"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/share/nano
