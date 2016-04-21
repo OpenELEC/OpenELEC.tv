bc6e4e3345a9eb1e6017513c9d3855f2341632be
Stephan Raue <stephan@openelec.tv>
2016-04-21 22:39:05 +0200

    hdhomerun: update to PKG_ADDON_REPOVERSION=8.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/driver/hdhomerun/package.mk
+++ packages/addons/official/driver/hdhomerun/package.mk
@@ -32,7 +32,7 @@ PKG_AUTORECONF="no"
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.python.script"
 PKG_ADDON_PROVIDES=""
-PKG_ADDON_REPOVERSION="7.0"
+PKG_ADDON_REPOVERSION="8.0"
 
 make_target() {
   : # nothing to do here
