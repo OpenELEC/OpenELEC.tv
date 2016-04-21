d2eae8c393ac420ecfe22c23dfff5b9242a92c27
Stephan Raue <stephan@openelec.tv>
2016-04-21 16:30:05 +0200

    inputstream.mpd: update to inputstream.mpd-4323ce6 from https://github.com/liberty-developer/inputstream.mpd, exclude from update_binary-addons script
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/kodi-addons/inputstream.mpd/package.mk
+++ packages/addons/official/kodi-addons/inputstream.mpd/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="inputstream.mpd"
-PKG_VERSION="6b058a0"
+PKG_VERSION="4323ce6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/mapfau/inputstream.mpd"
-PKG_GIT_URL="https://github.com/mapfau/inputstream.mpd"
+PKG_SITE="https://github.com/liberty-developer/inputstream.mpd"
+PKG_GIT_URL="https://github.com/liberty-developer/inputstream.mpd.git"
 PKG_GIT_BRANCH="master"
 PKG_DEPENDS_TARGET="toolchain kodi-platform expat"
 PKG_PRIORITY="optional"
@@ -42,8 +42,25 @@ configure_target() {
         ..
 }
 
+post_makeinstall_target() {
+  cd $ROOT/$PKG_BUILD/wvdecrypter
+  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DCMAKE_MODULE_PATH=$SYSROOT_PREFIX/usr/lib/kodi \
+        -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr \
+        .
+  make
+
+  mkdir -p $INSTALL/usr/lib/kodi/addons/$PKG_NAME/lib
+    cp -P libssd_wv.so $INSTALL/usr/lib/kodi/addons/$PKG_NAME/lib
+}
+
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
-  cp -PR $PKG_BUILD/.install_pkg/usr/share/kodi/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
-  cp -PL $PKG_BUILD/.install_pkg/usr/lib/kodi/addons/$PKG_NAME/*.so $ADDON_BUILD/$PKG_ADDON_ID/
+    cp -PR $PKG_BUILD/.install_pkg/usr/share/kodi/addons/$PKG_NAME/* $ADDON_BUILD/$PKG_ADDON_ID/
+    cp -PL $PKG_BUILD/.install_pkg/usr/lib/kodi/addons/$PKG_NAME/*.so $ADDON_BUILD/$PKG_ADDON_ID/
+
+   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib
+    cp -P $PKG_BUILD/wvdecrypter/libssd_wv.so $ADDON_BUILD/$PKG_ADDON_ID/lib
 }
+
