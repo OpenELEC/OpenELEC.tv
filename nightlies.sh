0283b1e1e5da77c4aa723fd390b5859ba6b6b1b7
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-04-21 21:27:33 +0200

    Build Generic images first

--- nightlies.sh
+++ nightlies.sh
@@ -2,13 +2,13 @@
 
 rm -rf target/
 
+DISTRO=Lakka PROJECT=Generic ARCH=x86_64 make image -j9
+DISTRO=Lakka PROJECT=Generic ARCH=i386 make image -j9
 DISTRO=Lakka PROJECT=RPi ARCH=arm make image -j9
 DISTRO=Lakka PROJECT=RPi ARCH=arm make noobs -j9
 DISTRO=Lakka PROJECT=RPi2 ARCH=arm make image -j9
 DISTRO=Lakka PROJECT=RPi2 ARCH=arm make noobs -j9
 DISTRO=Lakka PROJECT=imx6 ARCH=arm make image -j9
-DISTRO=Lakka PROJECT=Generic ARCH=x86_64 make image -j9
-DISTRO=Lakka PROJECT=Generic ARCH=i386 make image -j9
 DISTRO=Lakka PROJECT=a20 ARCH=arm make image -j9
 DISTRO=Lakka PROJECT=a10 ARCH=arm make image -j9
 DISTRO=Lakka PROJECT=Bananapi ARCH=arm make image -j9
