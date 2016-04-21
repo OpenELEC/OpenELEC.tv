e0b1f79c8a127f465c1e254dfafff4a6f93f813c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-04-21 13:53:57 +0200

    Update RetroArch. Display a PS3 like animated ribbon. Work in progress.

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="6f63305"
+PKG_VERSION="39305b9"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
