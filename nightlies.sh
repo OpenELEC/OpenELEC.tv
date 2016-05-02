b2e76de7032b92a8fc433b97b13a455f62754fcd
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-05-02 11:31:22 +0200

    Print the name of the build in the nightly script. Ease the detection of mkimage errors.

--- nightlies.sh
+++ nightlies.sh
@@ -2,20 +2,35 @@
 
 rm -rf target/
 
+>&2 echo "Generic.x86_64"
 DISTRO=Lakka PROJECT=Generic ARCH=x86_64 make image -j9
+>&2 echo "Generic.i386"
 DISTRO=Lakka PROJECT=Generic ARCH=i386 make image -j9
+>&2 echo "RPi.arm"
 DISTRO=Lakka PROJECT=RPi ARCH=arm make image -j9
+>&2 echo "RPi.arm noobs"
 DISTRO=Lakka PROJECT=RPi ARCH=arm make noobs -j9
+>&2 echo "RPi2.arm"
 DISTRO=Lakka PROJECT=RPi2 ARCH=arm make image -j9
+>&2 echo "RPi2.arm noobs"
 DISTRO=Lakka PROJECT=RPi2 ARCH=arm make noobs -j9
+>&2 echo "imx6.cuboxi.arm"
 DISTRO=Lakka PROJECT=imx6 SYSTEM=cuboxi ARCH=arm make image -j9
+>&2 echo "imx6.udoo.arm"
 DISTRO=Lakka PROJECT=imx6 SYSTEM=udoo ARCH=arm make image -j9
+>&2 echo "a20.arm"
 DISTRO=Lakka PROJECT=a20 ARCH=arm make image -j9
+>&2 echo "a10.arm"
 DISTRO=Lakka PROJECT=a10 ARCH=arm make image -j9
+>&2 echo "Bananapi.arm"
 DISTRO=Lakka PROJECT=Bananapi ARCH=arm make image -j9
+>&2 echo "OdroidC1.arm"
 DISTRO=Lakka PROJECT=OdroidC1 ARCH=arm make image -j9
+>&2 echo "OdroidXU3.arm"
 DISTRO=Lakka PROJECT=OdroidXU3 ARCH=arm make image -j9
+>&2 echo "WeTek_Play.arm"
 DISTRO=Lakka PROJECT=WeTek_Play ARCH=arm make image -j9
+>&2 echo "WeTek_Core.arm"
 DISTRO=Lakka PROJECT=WeTek_Core ARCH=arm make image -j9
 
 for f in target/*; do
