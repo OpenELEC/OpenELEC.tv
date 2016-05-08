0c2b455ddb1db2d2f4201368142d3433af280133
Stephan Raue <stephan@openelec.tv>
2016-05-08 11:01:14 +0200

    linux: update to linux-6006082 for RPi*, this fixes #4942
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -41,9 +41,9 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan"
     ;;
   rpi)
-    PKG_VERSION="7d73e63"
-    PKG_GIT_URL="https://github.com/raspberrypi/linux.git"
-    PKG_GIT_BRANCH="rpi-4.6.y"
+    PKG_VERSION="6006082"
+    PKG_GIT_URL="https://github.com/OpenELEC/linux.git"
+    PKG_GIT_BRANCH="raspberry-rpi-4.6.y"
     ;;
   *)
     PKG_VERSION="4.6-rc6"
