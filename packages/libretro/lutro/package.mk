f358f7dde2a3524034555afea0960d474a140e2c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-06-02 14:23:02 +0200

    Update Lutro

--- packages/libretro/lutro/package.mk
+++ packages/libretro/lutro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="lutro"
-PKG_VERSION="a19c2f1"
+PKG_VERSION="66cf08a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
