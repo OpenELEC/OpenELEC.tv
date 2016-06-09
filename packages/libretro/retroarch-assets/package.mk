d5a259a59b67a875b85218950e4e70301b0ded93
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-06-09 04:52:38 +0200

    Update assets, add Pixel theme

--- packages/libretro/retroarch-assets/package.mk
+++ packages/libretro/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="70b7d1e"
+PKG_VERSION="4ce9174"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
