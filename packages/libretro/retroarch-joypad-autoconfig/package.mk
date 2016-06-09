bd067b2db6a984857f841f08a70a8fc5537a8e69
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-06-09 05:05:02 +0200

    Update joypads. Add Gamegirl config

--- packages/libretro/retroarch-joypad-autoconfig/package.mk
+++ packages/libretro/retroarch-joypad-autoconfig/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="199afdd"
+PKG_VERSION="8700c15"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
