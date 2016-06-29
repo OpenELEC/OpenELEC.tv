b87f62db8c13409b2261dfec82a036afb278485a
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-06-29 13:56:03 +0200

    Update fceumm, reduce input lag

--- packages/libretro/fceumm/package.mk
+++ packages/libretro/fceumm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fceumm"
-PKG_VERSION="479d5d8"
+PKG_VERSION="368f2e6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
