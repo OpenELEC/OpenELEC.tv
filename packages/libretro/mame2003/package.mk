83c560ec754b057070cd9a31d4f09b94d17200d3
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-07-10 14:10:20 +0200

    Update mame2003

--- packages/libretro/mame2003/package.mk
+++ packages/libretro/mame2003/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mame2003"
-PKG_VERSION="4145a3b"
+PKG_VERSION="ae84f5d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
