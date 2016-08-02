ddf9795ce504cf5cf4e4df458865f275cd9dfa37
Stephan Raue <stephan@openelec.tv>
2016-08-02 09:54:48 +0200

    inputstream.mpd: update to inputstream.mpd-1a4ca75
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/kodi-addons/inputstream.mpd/package.mk
+++ packages/addons/official/kodi-addons/inputstream.mpd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="inputstream.mpd"
-PKG_VERSION="4323ce6"
+PKG_VERSION="1a4ca75"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
