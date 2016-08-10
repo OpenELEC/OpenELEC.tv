ce48d66d0c9f957fe0612333b0830d475e935a80
Stephan Raue <stephan@openelec.tv>
2016-08-10 23:36:31 +0200

    llvm: change to use cmake buildsystem
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -19,13 +19,13 @@
 PKG_NAME="llvm"
 PKG_VERSION="3.8.1"
 PKG_REV="1"
-PKG_ARCH="any"
+PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
 PKG_URL="http://llvm.org/releases/$PKG_VERSION/${PKG_NAME}-${PKG_VERSION}.src.tar.xz"
 PKG_SOURCE_DIR="${PKG_NAME}-${PKG_VERSION}.src"
-PKG_DEPENDS_HOST=""
-PKG_DEPENDS_TARGET="toolchain llvm:host"
+PKG_DEPENDS_HOST="toolchain"
+PKG_DEPENDS_TARGET="toolchain llvm:host zlib"
 PKG_PRIORITY="optional"
 PKG_SECTION="lang"
 PKG_SHORTDESC="llvm: Low Level Virtual Machine"
@@ -34,88 +34,65 @@ PKG_LONGDESC="Low-Level Virtual Machine (LLVM) is a compiler infrastructure desi
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-# package specific configure options
-PKG_CONFIGURE_OPTS_HOST="--disable-polly \
-                         --disable-libcpp \
-                         --disable-cxx11 \
-                         --disable-split-dwarf \
-                         --disable-clang-arcmt \
-                         --disable-clang-static-analyzer \
-                         --disable-clang-rewriter \
-                         --disable-zlib \
-                         --disable-assertions \
-                         --disable-werror \
-                         --disable-terminfo \
-                         --enable-optimized \
-                         --disable-debug-runtime \
-                         --disable-debug-symbols \
-                         --enable-keep-symbols \
-                         --enable-targets=r600"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-polly \
-                           --disable-libcpp \
-                           --disable-cxx11 \
-                           --disable-split-dwarf \
-                           --disable-clang-arcmt \
-                           --disable-clang-static-analyzer \
-                           --disable-clang-rewriter \
-                           --enable-optimized \
-                           --disable-profiling \
-                           --disable-assertions \
-                           --disable-werror \
-                           --disable-terminfo \
-                           --disable-expensive-checks \
-                           --disable-debug-runtime \
-                           --disable-debug-symbols \
-                           --enable-keep-symbols \
-                           --enable-jit \
-                           --disable-docs \
-                           --disable-doxygen \
-                           --enable-threads \
-                           --enable-pthreads \
-                           --enable-zlib \
-                           --enable-pic \
-                           --enable-shared \
-                           --enable-embed-stdcxx \
-                           --enable-timestamps \
-                           --disable-backtraces \
-                           --disable-libffi \
-                           --disable-ltdl-install"
-
-if [ "$TARGET_ARCH" = x86_64 ]; then
-  PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_TARGET --enable-targets=x86_64,r600"
-elif [ "$TARGET_ARCH" = arm ]; then
-  PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_TARGET --enable-targets=arm"
-fi
-
-PKG_MAKE_OPTS_HOST="BUILD_DIRS_ONLY=1 CFLAGS= CXXFLAGS="
-
-pre_configure_host() {
-  ( cd ../autoconf
-    aclocal  --force --verbose -I m4
-    autoconf --force --verbose -I m4 -o ../configure
-  )
-
-  # we are building hosttools inside the target builddir
-    mkdir -p ../.$TARGET_NAME && cd ../.$TARGET_NAME/
-    rm -rf ../.$HOST_NAME
-    mkdir -p BuildTools && cd BuildTools
+configure_host() {
+  cmake -DCMAKE_INSTALL_PREFIX=$ROOT/$TOOLCHAIN \
+        -DCMAKE_BUILD_TYPE=Release \
+        $LLVM_OPTS \
+        -DLLVM_BUILD_TOOLS=ON \
+        -DLLVM_INCLUDE_TOOLS=ON \
+        -DLLVM_BUILD_EXAMPLES=OFF \
+        -DLLVM_INCLUDE_EXAMPLES=OFF \
+        -DLLVM_BUILD_TESTS=OFF \
+        -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_BUILD_DOCS=OFF \
+        -DLLVM_ENABLE_DOXYGEN=OFF \
+        -DLLVM_ENABLE_ZLIB=OFF \
+        -DLLVM_ENABLE_TERMINFO=OFF \
+        -DLLVM_OPTIMIZED_TABLEGEN=ON \
+        -DLLVM_TARGETS_TO_BUILD=AMDGPU \
+        ..
 }
 
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -fPIC"
-  export CXXFLAGS="$CXXFLAGS -fPIC"
+configure_target() {
+  cmake -DCMAKE_INSTALL_PREFIX=/usr \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_SYSTEM_NAME=Linux \
+        -DLIBCXX_INSTALL_LIBRARY=OFF \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DLLVM_NATIVE_ARCH=X86 \
+        -DLLVM_DEFAULT_TARGET_TRIPLE=$TARGET_NAME \
+        -DLLVM_HOST_TRIPLE=$HOST_NAME \
+        -DLLVM_BUILD_TOOLS=OFF \
+        -DLLVM_INCLUDE_TOOLS=OFF \
+        -DLLVM_BUILD_EXAMPLES=OFF \
+        -DLLVM_INCLUDE_EXAMPLES=OFF \
+        -DLLVM_BUILD_TESTS=OFF \
+        -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_BUILD_DOCS=OFF \
+        -DLLVM_INCLUDE_DOCS=OFF \
+        -DLLVM_INSTALL_UTILS=OFF \
+        -DLLVM_INCLUDE_UTILS=OFF \
+        -DLLVM_ENABLE_DOXYGEN=OFF \
+        -DLLVM_ENABLE_ZLIB=ON \
+        -DLLVM_ENABLE_TERMINFO=OFF \
+        -DLLVM_TABLEGEN=$ROOT/$TOOLCHAIN/bin/llvm-tblgen \
+        -DLLVM_ENABLE_BACKTRACES=OFF \
+        -DLLVM_OPTIMIZED_TABLEGEN=ON \
+        -DLLVM_TARGETS_TO_BUILD=AMDGPU \
+        ..
+}
+
+make_host() {
+  make llvm-tblgen llvm-config
 }
 
 makeinstall_host() {
-# nothing to install here
- :
+  cp -PR bin/llvm-config $SYSROOT_PREFIX/usr/bin/llvm-config-host
+  cp -PR bin/llvm-tblgen $ROOT/$TOOLCHAIN/bin
 }
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
-  rm -rf $INSTALL/usr/lib/BugpointPasses.so
-  rm -rf $INSTALL/usr/lib/LLVMHello.so
-  rm -rf $INSTALL/usr/lib/libLTO.so
-  rm -rf $INSTALL/usr/lib/libprofile_rt.so
+  rm -rf $INSTALL/usr/lib
+  rm -rf $INSTALL/usr/share
 }
