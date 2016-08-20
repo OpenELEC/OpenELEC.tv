2cf7573b5f992bc6c994808548894dbf80cb6c5a
Floréal Toumikian <floreal@nimukaito.net>
2016-08-20 11:44:58 +0200

    Updated: core-info package

--- packages/libretro/core-info/package.mk
+++ packages/libretro/core-info/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="20b3cde"
+PKG_VERSION="2bde92f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
