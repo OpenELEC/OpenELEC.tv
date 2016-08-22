255ddc0624a64d8201c5651e99e4cf1239c972b8
Floréal Toumikian <floreal@nimukaito.net>
2016-08-22 18:34:37 +0200

    Updated: beetle-psx package

--- packages/libretro/beetle-psx/package.mk
+++ packages/libretro/beetle-psx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-psx"
-PKG_VERSION="d9dd6c2"
+PKG_VERSION="ec5fa68"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
