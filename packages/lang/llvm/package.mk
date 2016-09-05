0e149e2bb371bc7415258ca1e404e5fb54cf80bc
Stephan Raue <stephan@openelec.tv>
2016-09-05 22:40:39 +0200

    llvm: update to llvm-3.9.0, build as shared lib
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="llvm"
-PKG_VERSION="3.8.1"
+PKG_VERSION="3.9.0"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
@@ -58,12 +58,11 @@ configure_target() {
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_SYSTEM_NAME=Linux \
         -DLIBCXX_INSTALL_LIBRARY=OFF \
-        -DBUILD_SHARED_LIBS=OFF \
         -DLLVM_NATIVE_ARCH=X86 \
         -DLLVM_DEFAULT_TARGET_TRIPLE=$TARGET_NAME \
         -DLLVM_HOST_TRIPLE=$HOST_NAME \
         -DLLVM_BUILD_TOOLS=OFF \
-        -DLLVM_INCLUDE_TOOLS=OFF \
+        -DLLVM_INCLUDE_TOOLS=ON \
         -DLLVM_BUILD_EXAMPLES=OFF \
         -DLLVM_INCLUDE_EXAMPLES=OFF \
         -DLLVM_BUILD_TESTS=OFF \
@@ -78,7 +77,9 @@ configure_target() {
         -DLLVM_TABLEGEN=$ROOT/$TOOLCHAIN/bin/llvm-tblgen \
         -DLLVM_ENABLE_BACKTRACES=OFF \
         -DLLVM_OPTIMIZED_TABLEGEN=ON \
-        -DLLVM_TARGETS_TO_BUILD=AMDGPU \
+        -DLLVM_TARGETS_TO_BUILD="AMDGPU" \
+        -DLLVM_BUILD_LLVM_DYLIB=ON \
+        -DLLVM_LINK_LLVM_DYLIB=ON \
         ..
 }
 
@@ -93,6 +94,7 @@ makeinstall_host() {
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
-  rm -rf $INSTALL/usr/lib
+  rm -rf $INSTALL/usr/lib/LLVMHello.so
+  rm -rf $INSTALL/usr/lib/libLTO.so
   rm -rf $INSTALL/usr/share
 }
