86cd9d17b18718ed9644fa92b65e854b35f2e2d6
Stephan Raue <stephan@openelec.tv>
2016-09-12 09:11:09 +0200

    linux: update to linux-4.4.20
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -40,12 +40,12 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan"
     ;;
   rpi)
-    PKG_VERSION="5ba1281"
+    PKG_VERSION="73bada5"
     PKG_GIT_URL="https://github.com/raspberrypi/linux.git"
     PKG_GIT_BRANCH="rpi-4.4.y"
     ;;
   *)
-    PKG_VERSION="4.4.19"
+    PKG_VERSION="4.4.20"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
 esac
