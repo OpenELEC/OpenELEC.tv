a65b0c6f5999c45dfc88a67b7f539114f11968c7
Stephan Raue <stephan@openelec.tv>
2016-09-12 21:59:50 +0200

    initramfs: switch to psplash
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -172,7 +172,6 @@ makeinstall_init() {
   mkdir -p $INSTALL/lib
     cp -PR $ROOT/$PKG_BUILD/.$TARGET_NAME/elf/ld*.so* $INSTALL/lib
     cp $ROOT/$PKG_BUILD/.$TARGET_NAME/libc.so.6 $INSTALL/lib
-    cp $ROOT/$PKG_BUILD/.$TARGET_NAME/math/libm.so* $INSTALL/lib
     cp $ROOT/$PKG_BUILD/.$TARGET_NAME/nptl/libpthread.so.0 $INSTALL/lib
     cp -PR $ROOT/$PKG_BUILD/.$TARGET_NAME/rt/librt.so* $INSTALL/lib
 
