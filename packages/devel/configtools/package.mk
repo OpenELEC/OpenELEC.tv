89d6dd9b16f35f43ac1406f3f18ee2ae0788eb98
Stephan Raue <stephan@openelec.tv>
2016-09-12 09:37:50 +0200

    configtools: update to configtools-f41aabd
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="configtools"
-PKG_VERSION="706fbe5"
+PKG_VERSION="f41aabd"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
