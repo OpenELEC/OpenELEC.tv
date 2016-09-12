7e470efffe5668ce818437da6da7dc6c979ba817
Stephan Raue <stephan@openelec.tv>
2016-09-12 09:55:13 +0200

    libnl: update to libnl-3.2.28
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/libnl/package.mk
+++ packages/network/libnl/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libnl"
-PKG_VERSION="3.2.27"
+PKG_VERSION="3.2.28"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
