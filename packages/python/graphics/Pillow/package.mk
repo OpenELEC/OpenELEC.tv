19e5e8144952f3f6f50a5f4bd3524ecc1e7e42ca
Stephan Raue <stephan@openelec.tv>
2016-09-12 10:03:31 +0200

    Pillow: update to Pillow-3.3.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/python/graphics/Pillow/package.mk
+++ packages/python/graphics/Pillow/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="Pillow"
-PKG_VERSION="3.2.0"
+PKG_VERSION="3.3.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.pythonware.com/products/pil/"
-PKG_URL="https://pypi.python.org/packages/source/P/Pillow/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://pypi.python.org/packages/7b/32/e6978aed8fba6398c00eec25f12798fb57647a6cef4f85db1567e3d086ef/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python distutilscross:host zlib freetype libjpeg-turbo tiff"
 PKG_PRIORITY="optional"
 PKG_SECTION="python"
@@ -38,7 +38,7 @@ pre_make_target() {
 }
 
 make_target() {
-  python setup.py build --cross-compile
+  python setup.py build_ext --disable-platform-guessing
 }
 
 makeinstall_target() {
