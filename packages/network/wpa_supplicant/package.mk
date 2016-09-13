6064ea0c319c5e1470645b83330b64ef581415b7
Stephan Raue <stephan@openelec.tv>
2016-09-13 18:28:33 +0200

    wpa_supplicant: use libnl-tiny as libnl implementation
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://hostap.epitest.fi/wpa_supplicant/"
 PKG_URL="http://hostap.epitest.fi/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain dbus libnl libressl"
+PKG_DEPENDS_TARGET="toolchain dbus libnl-tiny libressl"
 PKG_PRIORITY="optional"
 PKG_SECTION="network"
 PKG_SHORTDESC="wpa_supplicant: An IEEE 802.11i supplicant implementation"
@@ -37,7 +37,7 @@ PKG_MAKEINSTALL_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/b
 
 configure_target() {
   LDFLAGS="$LDFLAGS -lpthread -lm"
-
+  CFLAGS="$CFLAGS -D_GNU_SOURCE -DCONFIG_LIBNL20 -I$SYSROOT_PREFIX/usr/include/libnl-tiny"
   cp $PKG_DIR/config/makefile.config wpa_supplicant/.config
 
 # echo "CONFIG_TLS=gnutls" >> .config
