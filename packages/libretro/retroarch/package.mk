fd94702a07ac65d3dc90c8e68ff25e76f9220f07
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-09-14 11:55:50 +0200

    Update RetroArch to latest master

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="397f82c"
+PKG_VERSION="6ff07a0"
 PKG_REV="5"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
