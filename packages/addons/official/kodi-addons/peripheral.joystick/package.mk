d43634cc869a5f6d62394a2055467bc243dd6e77
Stephan Raue <stephan@openelec.tv>
2016-09-18 21:28:20 +0200

    peripheral.joystick: simplyfing cmake configuration
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/kodi-addons/peripheral.joystick/package.mk
+++ packages/addons/official/kodi-addons/peripheral.joystick/package.mk
@@ -18,12 +18,6 @@
 
 PKG_NAME="peripheral.joystick"
 PKG_VERSION="263aa84"
-
-# temporary hack until kodi is updated
-if [ "$KODIPLAYER_DRIVER" = bcm2835-firmware ]; then
-  PKG_VERSION="263aa84"
-fi
-
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -40,13 +34,8 @@ PKG_AUTORECONF="no"
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="kodi.peripheral"
 
-configure_target() {
-  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        -DCMAKE_MODULE_PATH=$SYSROOT_PREFIX/usr/lib/kodi \
-        -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr \
-        ..
-}
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_MODULE_PATH=$SYSROOT_PREFIX/usr/lib/kodi \
+                       -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr"
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
