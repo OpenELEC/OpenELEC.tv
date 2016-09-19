8679e5a3e1c667fce2c194d01cd631010bd514c2
Stephan Raue <stephan@openelec.tv>
2016-09-19 21:29:36 +0200

    tinyxml: build with '--with-pic', cleanup
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/textproc/tinyxml/package.mk
+++ packages/textproc/tinyxml/package.mk
@@ -32,11 +32,7 @@ PKG_LONGDESC="TinyXML is a simple, small, C++ XML parser that can be easily inte
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
-
-pre_configure_target() {
-  export CFLAGS="$CFLAGS -fPIC"
-}
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-pic"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr
