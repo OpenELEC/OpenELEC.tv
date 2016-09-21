aded3b1ffa8a8b2c9f290f51c5ea03b0868478e2
Jernej Skrabec <jernej.skrabec@siol.net>
2016-09-22 00:02:42 +0200

    sunxi-tools: Compile it also for target

--- packages/tools/sunxi-tools/package.mk
+++ packages/tools/sunxi-tools/package.mk
@@ -34,11 +34,24 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_host() {
+  make clean
   make fex2bin
+}
+
+make_target() {
+  make clean
+  make CC="$TARGET_CC" fex2bin
+  make CC="$TARGET_CC" bin2fex
 }
 
 makeinstall_host() {
   cp -PR fex2bin $ROOT/$TOOLCHAIN/bin/
   cp -PR sunxi-fexc $ROOT/$TOOLCHAIN/bin/
 }
-	
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+  cp -PR fex2bin $INSTALL/usr/bin
+  cp -PR bin2fex $INSTALL/usr/bin
+  cp -PR sunxi-fexc $INSTALL/usr/bin
+}
