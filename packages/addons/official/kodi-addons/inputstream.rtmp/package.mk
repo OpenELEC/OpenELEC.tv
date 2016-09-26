13e1e1c0d1ec308ef391cfc35557461484337e1d
Stephan Raue <stephan@openelec.tv>
2016-09-26 08:25:25 +0200

    kodi-addons: update kodi binary addons
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/kodi-addons/inputstream.rtmp/package.mk
+++ packages/addons/official/kodi-addons/inputstream.rtmp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="inputstream.rtmp"
-PKG_VERSION="91bb374"
+PKG_VERSION="70cb86f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
--- packages/addons/official/kodi-addons/peripheral.joystick/package.mk
+++ packages/addons/official/kodi-addons/peripheral.joystick/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="peripheral.joystick"
-PKG_VERSION="263aa84"
+PKG_VERSION="abff316"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
