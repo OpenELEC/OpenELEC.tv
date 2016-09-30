ba0c9d04d4abc0ec1321a152e4e8332c5bef07cc
Jernej Skrabec <jernej.skrabec@siol.net>
2016-09-30 16:10:32 +0200

    wlan-firmware: Update version

--- packages/linux-firmware/wlan-firmware/package.mk
+++ packages/linux-firmware/wlan-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wlan-firmware"
-PKG_VERSION="4eea440"
+PKG_VERSION="34d66e8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
