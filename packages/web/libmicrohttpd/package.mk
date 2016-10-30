078c4727b6c3fcd15b86de59eaf3ff812607832e
Stephan Raue <stephan@openelec.tv>
2016-10-30 10:36:47 +0100

    libmicrohttpd: update to libmicrohttpd-0.9.52
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/web/libmicrohttpd/package.mk
+++ packages/web/libmicrohttpd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libmicrohttpd"
-PKG_VERSION="0.9.51"
+PKG_VERSION="0.9.52"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1"
