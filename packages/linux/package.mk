baf8bc1c880066c97d1d86ae4f15d0f89d001d60
Stephan Raue <stephan@openelec.tv>
2016-11-05 12:22:58 +0100

    linux: update to linux-4.9-rc3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -53,15 +53,20 @@ case "$LINUX" in
     PKG_PATCH_DIRS="linux-4.4 imx6-4.4"
     ;;
   rpi)
-    PKG_VERSION="7ede3d57"
+    PKG_VERSION="74fdf71"
     PKG_GIT_URL="https://github.com/OpenELEC/linux.git"
-    PKG_GIT_BRANCH="raspberry-rpi-4.8.y"
+    PKG_GIT_BRANCH="raspberry-rpi-4.9.y"
+    PKG_PATCH_DIRS="linux-4.9"
+    ;;
+  linux-4.8)
+    PKG_VERSION="4.8.6"
+    PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="linux-4.8"
     ;;
   *)
-    PKG_VERSION="4.8.5"
-    PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
-    PKG_PATCH_DIRS="linux-4.8"
+    PKG_VERSION="4.9-rc3"
+    PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/testing/$PKG_NAME-$PKG_VERSION.tar.xz"
+    PKG_PATCH_DIRS="linux-4.9"
     ;;
 esac
 
