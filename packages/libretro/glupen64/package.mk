16c6f02c1122ab2ba4b85367cafe979fbd55386b
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-11-23 16:56:09 +0100

    Fix glupen64 on RPi

--- packages/libretro/glupen64/package.mk
+++ packages/libretro/glupen64/package.mk
@@ -42,9 +42,13 @@ pre_configure_target() {
 make_target() {
   case $PROJECT in
     RPi|Gamegirl)
+      CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
+	              -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
       make platform=rpi
       ;;
     RPi2)
+      CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
+                      -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
       make platform=rpi2
       ;;
     Generic)
