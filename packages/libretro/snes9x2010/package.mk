a094e8d2c61b141a49aaf2f36fd19053a1d003fe
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-11-24 20:47:21 +0100

    Update snes9x2010

--- packages/libretro/snes9x2010/package.mk
+++ packages/libretro/snes9x2010/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x2010"
-PKG_VERSION="906065d"
+PKG_VERSION="74953d8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
