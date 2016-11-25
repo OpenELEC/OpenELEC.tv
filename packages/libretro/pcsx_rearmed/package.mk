320ea9f945e61b2a920041b6430fd994bb3e88d0
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-11-25 15:12:21 +0100

    Update pcsx, fixes analog sticks ranges

--- packages/libretro/pcsx_rearmed/package.mk
+++ packages/libretro/pcsx_rearmed/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="bb22137"
+PKG_VERSION="1aa232f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
