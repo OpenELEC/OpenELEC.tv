92843921e0cd7144020a105dbcd551cb2a91ea82
Stephan Raue <stephan@openelec.tv>
2016-11-26 22:41:43 +0100

    glibc: remove unneeded libs
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -135,6 +135,12 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/lib/*.o
   rm -rf $INSTALL/var
 
+# remove unneeded libs
+  rm -rf $INSTALL/usr/lib/libBrokenLocale*
+  rm -rf $INSTALL/usr/lib/libSegFault.so
+  rm -rf $INSTALL/usr/lib/libmemusage.so
+  rm -rf $INSTALL/usr/lib/libpcprofile.so
+
 # remove ldscripts
   rm -rf $INSTALL/usr/lib/libc.so
   rm -rf $INSTALL/usr/lib/libpthread.so
