81bee9edbf9ed68211007379df46653e3da47d9b
Stephan Raue <stephan@openelec.tv>
2016-11-27 02:05:28 +0100

    ffmpeg: update to ffmpeg-3.2.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ffmpeg"
-PKG_VERSION="3.2"
+PKG_VERSION="3.2.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1+"
