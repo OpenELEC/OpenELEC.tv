1dedd3029cbbb2ff26157ce7bf2b1170d6edba3c
Stephan Raue <stephan@openelec.tv>
2016-12-01 09:38:06 +0100

    linux: update to linux-4.9-rc7
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -64,7 +64,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="linux-4.8"
     ;;
   *)
-    PKG_VERSION="4.9-rc6"
+    PKG_VERSION="4.9-rc7"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/testing/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="linux-4.9"
     ;;
