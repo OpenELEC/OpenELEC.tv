5a482763547737e7799ec11cb763c6cefe54b2b8
Stephan Raue <stephan@openelec.tv>
2016-12-02 10:43:59 +0100

    cmake: update to cmake-3.7.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="cmake"
-PKG_VERSION="3.7.0"
+PKG_VERSION="3.7.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
