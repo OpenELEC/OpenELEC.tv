606f602d41909979d800c3e85cff10d6c7afb266
Stephan Raue <stephan@openelec.tv>
2016-12-18 11:30:47 +0100

    Revert "ccache: use libz from buildsystem"
    This reverts commit ae16a42337d2d0fb7bb6a7d211a440a7a859731b.
    
    Conflicts:
    	packages/devel/ccache/package.mk

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://ccache.samba.org/"
 PKG_URL="http://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_HOST="make:host libz:host"
+PKG_DEPENDS_HOST="make:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
 PKG_SHORTDESC="ccache: A fast compiler cache"
@@ -31,6 +31,8 @@ PKG_LONGDESC="Ccache is a compiler cache. It speeds up re-compilation of C/C++ c
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib"
 
 post_makeinstall_host() {
 # setup ccache
