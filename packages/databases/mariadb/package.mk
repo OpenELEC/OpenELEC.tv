160e303b4dce0a7213af79df8208a81d1bf5121b
Stephan Raue <stephan@openelec.tv>
2016-12-22 20:14:55 +0100

    mariadb: update to mariadb-10.1.20
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/databases/mariadb/package.mk
+++ packages/databases/mariadb/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mariadb"
-PKG_VERSION="10.1.17"
+PKG_VERSION="10.1.20"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
