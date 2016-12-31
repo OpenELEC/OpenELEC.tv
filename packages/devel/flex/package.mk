6ce156ce6b63e9ae07b12c30813d41aa46d69610
Stephan Raue <stephan@openelec.tv>
2016-12-31 14:35:41 +0100

    flex: update to flex-2.6.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/flex/package.mk
+++ packages/devel/flex/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="flex"
-PKG_VERSION="2.6.0"
+PKG_VERSION="2.6.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://flex.sourceforge.net/"
-PKG_URL="$SOURCEFORGE_SRC/flex/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://github.com/westes/flex/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
