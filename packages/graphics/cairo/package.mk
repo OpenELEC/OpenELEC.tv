5e55d481f77b034ed78d036b3220ae3dabd697f2
Stephan Raue <stephan@openelec.tv>
2017-01-02 12:30:19 +0100

    cairo: update to cairo-1.14.8
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/cairo/package.mk
+++ packages/graphics/cairo/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="cairo"
-PKG_VERSION="1.14.6"
+PKG_VERSION="1.14.8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
