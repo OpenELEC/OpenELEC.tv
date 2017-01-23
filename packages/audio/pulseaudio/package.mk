41f835752f75bdfd618f4407452cab619ca37a60
Stephan Raue <stephan@openelec.tv>
2017-01-23 11:43:58 +0100

    pulseaudio: update to pulseaudio-10.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/pulseaudio/package.mk
+++ packages/audio/pulseaudio/package.mk
@@ -17,13 +17,13 @@
 ################################################################################
 
 PKG_NAME="pulseaudio"
-PKG_VERSION="9.0"
+PKG_VERSION="10.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://pulseaudio.org/"
 PKG_URL="http://www.freedesktop.org/software/pulseaudio/releases/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain libtool json-c alsa-lib libsndfile soxr dbus systemd libressl libcap"
+PKG_DEPENDS_TARGET="toolchain libtool alsa-lib libsndfile soxr dbus libressl libcap"
 PKG_PRIORITY="optional"
 PKG_SECTION="audio"
 PKG_SHORTDESC="pulseaudio: Yet another sound server for Unix"
