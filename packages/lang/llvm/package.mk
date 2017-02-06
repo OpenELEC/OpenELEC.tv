8d4df5f5e85ccc7b97c2fd98adf70e62a16994af
Stephan Raue <stephan@openelec.tv>
2017-02-06 14:12:41 +0100

    llvm: update to llvm-4.0.0rc1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -17,12 +17,13 @@
 ################################################################################
 
 PKG_NAME="llvm"
-PKG_VERSION="3.9.1"
+PKG_VERSION="4.0.0rc1"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
-PKG_URL="http://llvm.org/releases/$PKG_VERSION/${PKG_NAME}-${PKG_VERSION}.src.tar.xz"
+#PKG_URL="http://llvm.org/releases/$PKG_VERSION/${PKG_NAME}-${PKG_VERSION}.src.tar.xz"
+PKG_URL="http://llvm.org/pre-releases/4.0.0/rc1/${PKG_NAME}-${PKG_VERSION}.src.tar.xz"
 PKG_SOURCE_DIR="${PKG_NAME}-${PKG_VERSION}.src"
 PKG_DEPENDS_HOST="toolchain"
 PKG_DEPENDS_TARGET="toolchain llvm:host zlib"
