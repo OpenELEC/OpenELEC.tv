827e1257ddf60638dc128a73ee261f9a5de43d8f
Stephan Raue <stephan@openelec.tv>
2017-02-13 16:28:29 +0100

    pciutils: remove kmod support
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/pciutils/package.mk
+++ packages/sysutils/pciutils/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://mj.ucw.cz/pciutils.shtml"
 PKG_URL="http://www.kernel.org/pub/software/utils/pciutils/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain kmod systemd"
+PKG_DEPENDS_TARGET="toolchain systemd"
 PKG_PRIORITY="optional"
 PKG_SECTION="system"
 PKG_SHORTDESC="pciutils: Linux PCI Utilities"
@@ -39,7 +39,7 @@ make_target() {
        CROSS_COMPILE=${TARGET_NAME}- \
        HOST=$TARGET_ARCH-linux \
        $PKG_MAKE_OPTS \
-       ZLIB=no DNS=no LIBKMOD=yes HWDB=yes
+       ZLIB=no DNS=no LIBKMOD=no HWDB=yes
 }
 
 makeinstall_target() {
