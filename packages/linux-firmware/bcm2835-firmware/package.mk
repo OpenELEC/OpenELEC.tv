e3c589bbfca2facd47f6f04a2bf1fc2febaa6b06
Stephan Raue <stephan@openelec.tv>
2017-02-19 22:48:46 +0100

    bcm2835-firmware: update to bcm2835-firmware-4ff0432
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-firmware/bcm2835-firmware/package.mk
+++ packages/linux-firmware/bcm2835-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-firmware"
-PKG_VERSION="72b44d8"
+PKG_VERSION="4ff0432"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
