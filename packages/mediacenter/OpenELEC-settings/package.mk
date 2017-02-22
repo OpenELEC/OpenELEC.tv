456d0a234eef194bc3e6919e850317f501594b38
Stephan Raue <stephan@openelec.tv>
2017-02-22 06:20:45 +0100

    OpenELEC-settings: update to OpenELEC-settings-9d97b7d
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/OpenELEC-settings/package.mk
+++ packages/mediacenter/OpenELEC-settings/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="OpenELEC-settings"
-PKG_VERSION="0.8.0"
+PKG_VERSION="9d97b7d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
