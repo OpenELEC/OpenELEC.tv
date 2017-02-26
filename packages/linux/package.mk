b148554f75e701e3575c1993ebd83ef218e8438e
Stephan Raue <stephan@openelec.tv>
2017-02-26 15:51:12 +0100

    linux: update to linux-4.9.13
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -65,7 +65,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="linux-4.8"
     ;;
   *)
-    PKG_VERSION="4.9.12"
+    PKG_VERSION="4.9.13"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="linux-4.9"
     ;;
