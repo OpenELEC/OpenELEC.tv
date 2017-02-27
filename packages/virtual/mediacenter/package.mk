f292a03a9eb1b62d6aaeb0221347593b304ba21f
Stephan Raue <stephan@openelec.tv>
2017-02-27 11:41:16 +0100

    mediacenter: install peripheral.joystick per default
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/virtual/mediacenter/package.mk
+++ packages/virtual/mediacenter/package.mk
@@ -38,6 +38,7 @@ if [ "$MEDIACENTER" = "kodi" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET simplejson"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET pycrypto"
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xmlstarlet"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET peripheral.joystick"
 
 # other packages
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET OpenELEC-settings"
