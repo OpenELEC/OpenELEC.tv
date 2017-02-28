1ee4f4b69ad0eb5e0dbb2c4b53639f92e9858084
Stephan Raue <stephan@openelec.tv>
2017-02-28 13:02:31 +0100

    inputstream.rtmp: update to inputstream.rtmp-df64b9b
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/kodi-addons/inputstream.rtmp/package.mk
+++ packages/addons/official/kodi-addons/inputstream.rtmp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="inputstream.rtmp"
-PKG_VERSION="55437ac"
+PKG_VERSION="df64b9b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
