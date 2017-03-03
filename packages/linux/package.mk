49cf5f325e51e74a67615ba614f6ca9f7a12dbfb
Stephan Raue <stephan@openelec.tv>
2017-03-03 10:50:20 +0100

    linux: update to linux-935c7ce
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -54,7 +54,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="linux-4.8 imx6-4.8"
     ;;
   rpi)
-    PKG_VERSION="883de20"
+    PKG_VERSION="935c7ce"
     PKG_GIT_URL="https://github.com/OpenELEC/linux.git"
     PKG_GIT_BRANCH="raspberry-rpi-4.9.y"
     PKG_PATCH_DIRS="linux-4.9 rpi-4.9"
