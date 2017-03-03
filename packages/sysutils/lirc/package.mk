4c6ea0d9ae6bdcb4d4dbb4ceaf1c877edb273dc0
Stephan Raue <stephan@openelec.tv>
2017-03-03 17:59:52 +0100

    lirc: remove lirc_rpi support
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/lirc/package.mk
+++ packages/sysutils/lirc/package.mk
@@ -60,9 +60,6 @@ pre_make_target() {
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/pronto2lirc
 
-  mkdir -p $INSTALL/etc/lirc
-    cp $PKG_DIR/config/lircd.conf.rpi $INSTALL/etc/lirc
-
   mkdir -p $INSTALL/usr/lib/openelec
     cp $PKG_DIR/scripts/lircd_helper $INSTALL/usr/lib/openelec
 
