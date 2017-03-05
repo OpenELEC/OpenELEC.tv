a024742666f4c01be60ebc720aa844cfcae8658f
Stephan Raue <stephan@openelec.tv>
2017-03-05 16:55:12 +0100

    bluez: disable systemd support
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/bluez/package.mk
+++ packages/network/bluez/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.bluez.org/"
 PKG_URL="http://www.kernel.org/pub/linux/bluetooth/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain dbus glib readline netbsd-curses systemd"
+PKG_DEPENDS_TARGET="toolchain dbus glib readline netbsd-curses"
 PKG_PRIORITY="optional"
 PKG_SECTION="network"
 PKG_SHORTDESC="bluez: Bluetooth Tools and System Daemons for Linux."
@@ -39,7 +39,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-dependency-tracking \
                            --disable-cups \
                            --disable-obex \
                            --enable-client \
-                           --enable-systemd \
+                           --disable-systemd \
                            --enable-tools \
                            --enable-datafiles \
                            --disable-experimental \
@@ -69,7 +69,6 @@ pre_configure_target() {
 }
 
 post_makeinstall_target() {
-  rm -rf $INSTALL/usr/lib/systemd
   rm -rf $INSTALL/usr/bin/bccmd
   rm -rf $INSTALL/usr/bin/bluemoon
   rm -rf $INSTALL/usr/bin/ciptool
