0d6e9993d4182d56d0dab3729e87294aa990e7ac
Stephan Raue <stephan@openelec.tv>
2017-03-25 15:11:17 +0100

    new package: add package 'rapidjson'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

---
+++ packages/textproc/rapidjson/package.mk
@@ -0,0 +1,34 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="rapidjson"
+PKG_VERSION="v1.1.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="BSD"
+PKG_SITE="http://rapidjson.org/"
+PKG_GIT_URL="https://github.com/miloyip/rapidjson.git"
+PKG_GIT_BRANCH="master"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="textproc"
+PKG_SHORTDESC="rapidjson: A fast JSON parser/generator for C++ with both SAX/DOM style API"
+PKG_LONGDESC="rapidjson is a fast JSON parser/generator for C++ with both SAX/DOM style API"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
