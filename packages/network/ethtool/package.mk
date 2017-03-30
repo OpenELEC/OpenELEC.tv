0b00757ef8926213f5caa7ee2e20160b3535974c
Stephan Raue <stephan@openelec.tv>
2017-03-30 12:15:45 +0200

    ethtool: update to ethtool-4.10
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/ethtool/package.mk
+++ packages/network/ethtool/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ethtool"
-PKG_VERSION="4.8"
+PKG_VERSION="4.10"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
