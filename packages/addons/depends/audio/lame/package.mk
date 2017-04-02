855b8c42007f82a9c33c31cd2616bba114320c5c
Stephan Raue <stephan@openelec.tv>
2017-04-02 14:01:01 +0200

    lame: update license headers
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/depends/audio/lame/package.mk
+++ packages/addons/depends/audio/lame/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -32,7 +32,6 @@ PKG_LONGDESC="LAME originally stood for LAME Ain't an Mp3 Encoder. The goal of t
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-# package specific configure options
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --disable-shared \
                            --disable-nasm \
