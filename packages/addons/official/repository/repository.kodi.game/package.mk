fb17014e6a05534c0b85f878e5d4ba573e638fed
Stephan Raue <stephan@openelec.tv>
2017-04-02 12:39:49 +0200

    repository.kodi.game: update to repository.kodi.game-f5f7085
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/repository/repository.kodi.game/package.mk
+++ packages/addons/official/repository/repository.kodi.game/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="repository.kodi.game"
-PKG_VERSION="7480ae2"
+PKG_VERSION="f5f7085"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
