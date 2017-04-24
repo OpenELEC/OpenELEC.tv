0df9a6a22b44ab61f2fcdaf7d77590d4d87246c7
Stephan Raue <stephan@openelec.tv>
2017-04-24 09:48:57 +0200

    vdr-addon: remove $PKG_ADDON_REPOVERSION
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/official/service/multimedia/vdr-addon/package.mk
+++ packages/addons/official/service/multimedia/vdr-addon/package.mk
@@ -29,13 +29,14 @@ PKG_PRIORITY="optional"
 PKG_SECTION="service.multimedia"
 PKG_SHORTDESC="vdr: A powerful DVB TV application"
 PKG_LONGDESC="This project describes how to build your own digital satellite receiver and video disk recorder. It is based mainly on the DVB-S digital satellite receiver card, which used to be available from Fujitsu Siemens and the driver software developed by the LinuxTV project."
+
 PKG_AUTORECONF="no"
+
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_PROVIDES=""
 PKG_ADDON_REQUIRES="pvr.vdr.vnsi:0.0.0 script.config.vdr:0.0.0"
 PKG_ADDON_NAME="VDR PVR Backend"
-PKG_ADDON_REPOVERSION="8.1"
 
 make_target() {
   : # nothing to do here
