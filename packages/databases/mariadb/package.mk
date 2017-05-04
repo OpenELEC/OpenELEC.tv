f0906ce5208d9edfc1b06ded721867a3a5dc4e86
Stephan Raue <stephan@openelec.tv>
2017-05-04 10:00:22 +0200

    mariadb: update to mariadb-10.1.23
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/databases/mariadb/package.mk
+++ packages/databases/mariadb/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mariadb"
-PKG_VERSION="10.1.22"
+PKG_VERSION="10.1.23"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
