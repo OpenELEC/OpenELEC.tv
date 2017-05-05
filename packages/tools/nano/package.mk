216abd572039a26ebf58572e35b557f6594ceb1d
Stephan Raue <stephan@openelec.tv>
2017-05-05 11:27:56 +0200

    nano: update to nano-2.8.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/tools/nano/package.mk
+++ packages/tools/nano/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="nano"
-PKG_VERSION="2.8.1"
+PKG_VERSION="2.8.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
