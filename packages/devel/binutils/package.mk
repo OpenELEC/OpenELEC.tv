7b9886e075987c80e8220a5437f852792004a96f
Mathieu Malaterre <malat@debian.org>
2017-05-23 21:38:07 +0200

    binutils: ci20 project requires an older binutils
    Signed-off-by: Mathieu Malaterre <malat@debian.org>

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -17,7 +17,15 @@
 ################################################################################
 
 PKG_NAME="binutils"
-PKG_VERSION="2.28"
+case "$LINUX" in
+  ci20)
+    # mxu patch requires 2.23:
+    PKG_VERSION="2.23.2"
+    ;;
+  *)
+    PKG_VERSION="2.28"
+    ;;
+esac
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
